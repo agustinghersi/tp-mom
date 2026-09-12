57ee6eec4afc48de25c6f08c7cdb3058a97f6442
agustinghersi <agusghersi@gmail.com>
2026-09-12 14:05:45 -0300

    Saco codigo del tutorial y lo adapto a los parametros

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -4,7 +4,6 @@ import string
 from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 import sys
 import os
-import time
 
 # Por defecto, RabbitMQ envía cada mensaje al siguiente consumidor, en secuencia. 
 # En promedio, cada consumidor recibe la misma cantidad de mensajes (Round Robin)
@@ -24,21 +23,11 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
             # Creacion de queue idempotente, conviene siempre hacerlo 2 veces
             channel.queue_declare(queue=self.queue_name, durable=True, arguments={'x-queue-type': 'quorum'})
-
-            # Utilizo la funcion callback que invoca pika para leer un mensaje de la cola
-            def callback(ch, method, properties, body):
-                pprint(f" [x] Received {body.decode()}")
-                # Simula 1 seegundo de trabajo por punto en el mensaje
-                time.sleep(body.count(b'.'))
-                print(" [x] Done")
-                # Ahora mando ack manual para confirm que el mensaje se proceso
-                # Ante un ctrl C el mensaje no se pierde
-                ch.basic_ack(delivery_tag = method.delivery_tag)
             
             channel.basic_qos(prefetch_count=1) # Hasta no terminar la tarea, Rabbit no envia otra al worker
             # Ver que esto puede dar error de llenar la queue despues
             channel.basic_consume(queue=self.queue_name,
-                        on_message_callback=callback) # Saco el ACK automatico
+                        on_message_callback=on_message_callback) # Saco el ACK automatico
 
             # Aca se entra en un bucle infinito, se sale con ctrl C
             print(' [*] Waiting for messages. To exit press CTRL+C')
@@ -62,15 +51,13 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
         # Declaro la queue a la que envio los mensajes
         channel.queue_declare(queue=self.queue_name, durable=True, arguments={'x-queue-type': 'quorum'})
 
-        # Mando un mensaje o el hola mundo
-        message = ' '.join(sys.argv[1:]) or "Hello World!"
+        # Mando el mensaje
         channel.basic_publish(exchange=self.exchange_name, # Ver que poner aca, no tengo el nombre
                       routing_key=self.routing_keys,
                       body=message,
                       properties=pika.BasicProperties( # Hago que los mensajes sean persistentes
                          delivery_mode = pika.DeliveryMode.Persistent # Ver el error de que queden en cache si pasa algo raro
                       ))
-        print(" [x] Sent 'Hello World!'")
 
         # Para vaciar buffers de red y garantizar envio de mensaje a rabbit
         connection.close()
